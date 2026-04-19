c24e0f8751ffc8d15bfc5ec96655220354f863d6
louis932858 <louispfennig12@gmail.com>
2026-04-19 10:57:05 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,27 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-HTML = """
-<h1>💎 NEXORA DASHBOARD</h1>
-<p>Bot Status: <b>ONLINE</b></p>
+def db():
+    conn = sqlite3.connect("shift.db")
+    c = conn.cursor()
+    return conn, c
 
-<hr>
-
-<h3>⚙️ System</h3>
-<ul>
-<li>Auto Moderation: ON</li>
-<li>Anti Spam: ON</li>
-<li>Voice Kick: ON</li>
-</ul>
-
-<p>Railway Deployment Active 🚀</p>
-"""
 
 @app.route("/")
 def home():
-    return HTML
+    conn, c = db()
+
+    c.execute("SELECT user_id, total_time FROM shifts ORDER BY total_time DESC")
+    data = c.fetchall()
+
+    leaderboard = []
+    for uid, t in data:
+        leaderboard.append({
+            "user_id": uid,
+            "hours": round(t / 3600, 2)
+        })
+
+    conn.close()
+    return render_template("index.html", leaderboard=leaderboard)
 
 
-def run():
+@app.route("/reset/<int:user_id>")
+def reset(user_id):
+    conn, c = db()
+    c.execute("UPDATE shifts SET total_time=0, start_time=NULL WHERE user_id=?", (user_id,))
+    conn.commit()
+    conn.close()
+    return "Reset done ✔"
+
+
+def run_dashboard():
     app.run(host="0.0.0.0", port=8080)
